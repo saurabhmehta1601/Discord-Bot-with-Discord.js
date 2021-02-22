0aaf03473109d7d84e860d7ab7c40fec4e4513d8
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-02-22 15:07:55 +0530

    user message and bot replies added

---
+++ botChats.js
@@ -0,0 +1,12 @@
+module.exports=[
+    {
+        statement:"hiiii",
+        reply:"I'm good"
+    },{
+        statement:"how are you?",
+        reply:"fine what about you"
+    },{
+        statement:"hii",
+        reply:"hellow bro"
+    }
+]
